faa8c0a828dd964faa03fdf965f5099cfe9713f6
Rohan Doshi <rohan.doshi02@gmail.com>
2021-10-14 11:15:15 +0530

    Update Assignment5.py

--- DSL_Lab/Assignment5.py
+++ DSL_Lab/Assignment5.py
@@ -14,7 +14,8 @@ class Sorting:
     def __init__(self):
         self.__length = 0
         self.__arr = []
-        self.__insertionList = []
+        self.__insertionSortList = []
+        self.__shellSortList = []
 
 
     def acceptList(self):
@@ -24,25 +25,50 @@ class Sorting:
             self.__arr.append(temp)
 
 
-    def insertionSort(self):
+    def insertionSort(self): #in each pass the list gets divided into 2 parts sorted and unsorted time complexity O(n^2)
         print("Current List is ", end=" ")
         printList(self.__arr)
-        self.__insertionList = self.__arr.copy()
+        self.__insertionSortList = self.__arr.copy()
         for j in range(1, self.__length):
-            temp = self.__insertionList[j]
-            self.Insert(temp, j-1)
+            temp = self.__insertionSortList[j]
+            self.Insert(temp, j-1) #pass value of j-1 to compare
+            print("Insert")
+            print(self.__insertionSortList)
 
-        print("Sorted List using insertion sort", end=" ")
-        printList(self.__insertionList)
+        print("\nSorted List using insertion sort", end=" ")
+        printList(self.__insertionSortList)
 
-    def Insert(self, temp, i):
-        while(temp<self.__insertionList[i] and i>=0):
-            self.__insertionList[i+1] = self.__insertionList[i]
+    def Insert(self, temp, i): #This is code to insert elements before the jth index in the consideration
+        while(temp<self.__insertionSortList[i] and i>=0):  #Here i>=0 is the last condition of the check
+            self.__insertionSortList[i+1] = self.__insertionSortList[i]
             i -=1
-        self.__insertionList[i+1] = temp
+        self.__insertionSortList[i+1] = temp
     
-    # def shellSort(self):
+    # following the gap = len/2 for shell sort so time complexity will be O(n^2)
+    def shellSort(self): 
+        self.__shellSortList = self.__arr.copy()
+        n = self.__length
+        gap = n//2
+        while gap > 0:  # loop(i = n/2; 0; i/=2)
+            j = gap
+            while j < n: #loop(j = gap; j<n; j++)
+                i = j - gap
+                while i>=0: #loop(i= j-gap; i>=0; i-=gap) 
+                    #This is to check say if gap is 2 then it will check for each gap previous values also
+                    if self.__shellSortList[i] > self.__shellSortList[i+gap]:
+                        switch(i+gap, i, self.__shellSortList)
+                        i = i-gap
+                    else:
+                        i = i-gap
+                        break
+                j +=1
+            gap = gap//2
+            print("Pass")
+            print(self.__shellSortList)
 
+        print("\nSorted List using shell sort", end=" ")
+        printList(self.__shellSortList)
+                    
 
 def printList(list1):
         print(list1)
@@ -55,4 +81,25 @@ def switch(pos1, pos2, list1):
 
 obj = Sorting()
 obj.acceptList()
-obj.insertionSort()
+
+while True:
+    print()
+    print("*" * 10 + "Menu" + "*" * 10)
+    print("""1 . Insertion Sort
+2. Shell Sort
+3. EXIT """)
+    print("*" * 24)
+    choice = int(input("Enter your choice for the following operations:"))
+
+    if choice == 1:
+        obj.insertionSort()
+
+    elif choice == 2:
+        obj.shellSort()
+
+    elif choice == 3:
+        print("EXIT")
+        break
+
+    else:
+        print("Enter valid input")
